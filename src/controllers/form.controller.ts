b37efcd87be635a1861271c9ed205fbc07c97158
hemant_sharma <hemantinsan6@gmail.com>
2025-12-11 14:24:31 +0530

    feat: refactor form fetching logic and implement form repository

--- src/controllers/form.controller.ts
+++ src/controllers/form.controller.ts
@@ -1,25 +1,12 @@
 import type { Request, Response } from "express";
 import { asyncHandler } from "../core/asyncHandler.js";
-import { prisma } from "../db/prisma.js";
 import { SuccessResponse } from "../core/ApiResponse.js";
 import type { QuestionnaireCategoryDTO } from "../types/form.js";
+import formRepository from "../db/repository/form.repository.js";
 
 const getForm = asyncHandler(async (_req: Request, res: Response) => {
-    const categories = await prisma.autismCategory.findMany({
-        orderBy:{id:'asc'},
-        include:{
-            questions:{
-                orderBy:{order:'asc'},
-                select:{
-                    id:true,
-                    question:true,
-                    weight:true,
-                    order:true
-                }
-            }
-        }
-    });
-    const payload: QuestionnaireCategoryDTO[] = categories.map((c) => ({
+   const form = await formRepository.getFormStructure();
+    const payload: QuestionnaireCategoryDTO[] = form.map((c) => ({
     id: c.id,
     name: c.name,
     questions: c.questions.map((q) => ({
@@ -34,9 +21,5 @@ const getForm = asyncHandler(async (_req: Request, res: Response) => {
     new SuccessResponse("Form fetched successfully", payload).send(res);
 })     
 
-const submitForm = asyncHandler(async (_req: Request, res: Response) => {
 
-
-    new SuccessResponse("Form submitted successfully",{}).send(res);
-});
-export default { getForm, submitForm };
+export default { getForm };
--- src/routes/form.routes.ts
+++ src/routes/form.routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
-// import { protect } from "../middlewares/auth.middleware.js";
+import { protect } from "../middlewares/auth.middleware.js";
 import formController from "../controllers/form.controller.js";
 
 const router = express.Router();
 
-// router.use(protect);
+router.use(protect);
 router.get("/form",formController.getForm);
 
-router.post("/form", formController.submitForm);
+
 
 export default router;
